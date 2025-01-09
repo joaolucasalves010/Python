f1961d5c33ed016b355e831207d8178ac841f97c
João Lucas <ylucasslalves@gmail.com>
2025-01-09 14:26:59 -0300

    os.system

--- 11 - os-shutil-request-response-json/1_os.py
+++ 11 - os-shutil-request-response-json/1_os.py
@@ -1,1 +1,10 @@
 import os
+os.system('cls') # os.system executa comandos direto no terminal
+os.system('echo "Hello World"')
+
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
